6cedd9197bf373984b0d2ff1e7f5f9d1ffe5b786
Daniel Colas <danielenrique93@gmail.com>
2021-12-20 16:28:07 +0100

    fix: pipeline

--- sql/migrations/21-add-constraint-skill-catalog-level.sql
+++ sql/migrations/21-add-constraint-skill-catalog-level.sql
@@ -1,5 +1,5 @@
-ALTER TABLE skills.skill_catalog_level
-  DROP COLUMN id;
+-- ALTER TABLE skills.skill_catalog_level
+--   DROP COLUMN id;
 
 -- ALTER TABLE skills.skill_catalog_level
 --   DROP CONSTRAINT skill_catalog_level_pkey;
@@ -7,9 +7,9 @@ ALTER TABLE skills.skill_catalog_level
 -- ALTER TABLE skills.skill_catalog_level
 --   ADD CONSTRAINT level_per_skill_uk UNIQUE ("level",skill_id);
 
-CREATE UNIQUE INDEX skill_catalog_level_skill_id_idx ON skills.skill_catalog_level (skill_id,"level");
+-- CREATE UNIQUE INDEX skill_catalog_level_skill_id_idx ON skills.skill_catalog_level (skill_id,"level");
 
-CREATE TRIGGER update_last_modification_date
-  BEFORE UPDATE ON skills.skill_catalog_level
-  FOR EACH ROW EXECUTE PROCEDURE update_last_modification_date();
+-- CREATE TRIGGER update_last_modification_date
+--   BEFORE UPDATE ON skills.skill_catalog_level
+--   FOR EACH ROW EXECUTE PROCEDURE update_last_modification_date();
 
