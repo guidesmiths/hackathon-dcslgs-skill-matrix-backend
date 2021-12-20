5e73d0de388b0b6adc92c72ae34ae4f09ffa76a5
Jennifer Goijman <jennifer.goijman@dcsl.com>
2021-12-20 16:12:29 +0100

    fix: migrations

--- sql/migrations/21-add-constraint-skill-catalog-level.sql
+++ sql/migrations/21-add-constraint-skill-catalog-level.sql
@@ -4,8 +4,8 @@ ALTER TABLE skills.skill_catalog_level
 -- ALTER TABLE skills.skill_catalog_level
 --   DROP CONSTRAINT skill_catalog_level_pkey;
 
-ALTER TABLE skills.skill_catalog_level
-  ADD CONSTRAINT level_per_skill_uk UNIQUE ("level",skill_id);
+-- ALTER TABLE skills.skill_catalog_level
+--   ADD CONSTRAINT level_per_skill_uk UNIQUE ("level",skill_id);
 
 CREATE UNIQUE INDEX skill_catalog_level_skill_id_idx ON skills.skill_catalog_level (skill_id,"level");
 
