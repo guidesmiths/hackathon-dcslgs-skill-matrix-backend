2589a9069c2983d9305b16e9820a43f08d370717
Jorge Adame <Jorge.Adame@dcsl.com>
2021-12-21 09:13:07 +0000

    Merged PR 40719: fix: migration with new ecosystem names
    ### Type:
    
    * [x] fix: bug, hotfix, etc.
    * [x] refactor: enhancements.
    
    ### What's the focus of this PR:
    - The focus of the PR is fix the migration with the new ecosystem names as _Spoken Language_ and _.NET_
    
    Related work items: #128556

--- sql/queries/select-skill-id.sql
+++ sql/queries/select-skill-id.sql
@@ -1,3 +1,3 @@
 SELECT sc.id FROM skills.skill_catalog sc left join skills.skill_ecosystem se ON se.id = sc.ecosystem
     WHERE sc."name" like ('%' || %L:skill_name) and sc.ecosystem in (SELECT id FROM skills.skill_ecosystem
-    where "name" = %L:ecosystem_name ) LIMIT 1
+    where "name" like ('%' || %L:ecosystem_name || '%')) LIMIT 1
