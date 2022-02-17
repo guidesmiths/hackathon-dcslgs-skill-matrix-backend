7c26172c3eca8bf4b6ea3eb0cbbb7baada283d87
Daniel Colas <danielenrique93@gmail.com>
2022-02-17 14:58:36 +0100

    refactor: update postgres component to add new features and new filtered answers query

--- components/postgres/initPostgres.js
+++ components/postgres/initPostgres.js
@@ -1,34 +1,6 @@
 const initHandyPg = require('handy-postgres');
 
-const { buildUpsertQuery } = require('./utils/queryBuilder');
-
-const getFiltersSubquery = ({ skills }) => {
-  let skillsLength = 0;
-  let text = '';
-
-  if (skills[0].skill) {
-    text += `us.user_id in (
-      select u.user_id from skills."user" u
-      left join skills.user_skill us on us.user_id = u.user_id
-      where`;
-
-    skills.forEach(skillLevel => {
-      if (skillLevel.skill) {
-        if (skillsLength > 0) { text += 'or'; }
-        text += ` (us.skill_id = ${skillLevel.skill} and us.skill_value >= ${skillLevel.level || 0}) `;
-        skillsLength += 1;
-      }
-    });
-
-    if (skillsLength > 0) {
-      text += `group by u.user_id
-      having count(*) = ${skillsLength}
-      ) and`;
-    }
-  }
-
-  return text;
-};
+const extendHandyPg = require('./extendPostgres');
 
 module.exports = ({ configPath }) => {
   let handyPg;
@@ -39,67 +11,57 @@ module.exports = ({ configPath }) => {
     }
     handyPg = initHandyPg({ logger, configPath });
     const pgAPI = await handyPg.start(config);
+    const extendedPgAPI = extendHandyPg(pgAPI);
+
+    handyPg = initHandyPg({ logger, configPath });
     return {
-      ...pgAPI,
+      ...extendedPgAPI,
       schema: config.schema,
 
-      /**
-       *  Execute an upsert operation on the specified table. The data passed
-       * down to this method may contain values for all of the columns in the
-       * table or just a subset of them: it's not required to pass the whole
-       * object if we only want to modify part of the row.
-       *
-       *  In these cases where some column names are missing from the `rowData`
-       * object keys, the following logic will apply:
-       *
-       *  - When inserting a new row, the default values will be used.
-       *
-       *  - When updating an existing row, the current value in that column
-       *    will be preserved.
-       *
-       * @param {string} tableName - Name of the table where the operation
-       *  will take place.
-       * @param {object} rowData - Object containing the values to insert.
-       *  The keys of the object must match existing column names in the
-       *  selected table.
-       */
-      upsert: (tableName, rowData, conflictConstraint) => {
-        const columnsWithValue = Object.keys(rowData)
-          .filter(key => rowData[key] !== undefined);
-
-        return pgAPI.query(
-          buildUpsertQuery(tableName, columnsWithValue, conflictConstraint),
-          columnsWithValue.map(key => rowData[key]),
-        );
-      },
-
-      fetchAnswersByUser: id => pgAPI.query(`
-        select u."name" as "userName", u.user_id as "userId", u.email, u."role" as "userRole", u.country, u.seniority,
-        us.skill_id as "skillId", sc."name" as "skillName", us.skill_value as "skillValue", us.skill_subvalue as "skillSubvalue", us.interested, us.comments,
-        se.id as "ecosystemId", se."name" as "ecosystemName", scl.description as "levelDescription"
-        from skills."user" u
-        left join skills.user_skill us on us.user_id = u.user_id
-        left join skills.skill_catalog sc on sc.id = us.skill_id
-        left join skills.skill_ecosystem se on se.id = sc.ecosystem
-        left join skills.skill_catalog_level scl on scl.skill_id = sc.id
-        where u.user_id = '${id}' and us.skill_value = scl.level
-      `),
-
-      fetchUsersFiltered: (filters, query, totalItems = false) => {
+      fetchUsersFiltered: async (filters, query) => {
         const usersPerPage = 10;
-        const { page, name } = query;
+        const { page, name = '' } = query;
+        const queryArgs = filters.skills?.map(({ skill, level }) => {
+          if (skill && level) {
+            return ([skill, level]);
+          }
+          return null;
+        }).flat().filter(Boolean) || [];
+        const amountOfFilters = Math.floor(queryArgs.length / 2);
+        const skillFilters = amountOfFilters && [...Array(amountOfFilters)].map((value, index) => {
+          if (index === 0) {
+            return `where (us.skill_id = $${index * 2 + 3} and us.skill_value >= $${index * 2 + 4})`;
+          }
+          return ` or (us.skill_id = $${index * 2 + 3} and us.skill_value >= $${index * 2 + 4})`;
+        });
+        const filterName = `%${name}%`;
 
-        return pgAPI.query(`
-          select u."name", u.user_id as "id", u.email, u."role", u.country, u.seniority, count(*) OVER() AS full_count
-          from skills."user" u
-          left join skills.user_skill us on us.user_id = u.user_id
-          left join skills.skill_catalog sc on sc.id = us.skill_id
-          where ${filters.skills?.length > 0 ? getFiltersSubquery(filters) : ''}
-          lower(u."name") like '%${name?.toLowerCase() || ''}%'
-          ${filters.skills?.[0]?.skill ? `and us.skill_id = ${filters.skills[0].skill}` : ''}
-          group by u.user_id ${filters.skills?.[0]?.skill ? ', us.skill_value order by us.skill_value desc' : ''}
-          ${!totalItems ? `LIMIT ${usersPerPage} OFFSET ${(page - 1) * usersPerPage};` : ''}
-        `);
+        const { rows: [{ count: total }] } = await pgAPI.query(`
+          select count(*) from (
+            select u."name", u.user_id as "id", u.email, u."role", u.country, u.seniority, row_number() over(partition by u.user_id) as rn
+              from skills.user_skill us 
+              left join skills."user" u on us.user_id = u.user_id 
+              ${amountOfFilters > 0 ? skillFilters.join('') : ''}
+            order by us.skill_value desc
+          ) as data
+          where data.rn = $2
+          and lower(data."name") like lower($1);
+        `, [filterName, amountOfFilters || 1, ...queryArgs]);
+
+        const { rows: users } = await pgAPI.query(`
+          select data.name, data.id, data.email, data.role, data.country, data.seniority from (
+            select u."name", u.user_id as "id", u.email, u."role", u.country, u.seniority, row_number() over(partition by u.user_id) as rn
+              from skills.user_skill us 
+              left join skills."user" u on us.user_id = u.user_id 
+              ${amountOfFilters > 0 ? skillFilters.join('') : ''}
+            order by us.skill_value desc
+          ) as data
+          where data.rn = $2
+          and lower(data."name") like lower($1)
+          limit ${page * usersPerPage} offset ${(page - 1) * usersPerPage};
+        `, [filterName, amountOfFilters || 1, ...queryArgs]);
+
+        return { total, users };
       },
 
     };
